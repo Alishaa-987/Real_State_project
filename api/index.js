c40fec8f054166cf06661ca325f18c922011b71d
Alisha Fatima <alishafatima6768@gmail.com>
2025-08-12 14:01:23 +0500

    connect the dataabase

--- api/index.js
+++ api/index.js
@@ -1,8 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+import dotenv from 'dotenv';
+dotenv.config({ path: './api/.env' }); // path is relative to where you run the script it is compulsory to define becaus i get a lot of error by not defining the path
+
+mongoose.connect(process.env.Connection)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 const app = express();
 
-app.listen(3000 , ()=>{
-    console.log("Server is running on port 3000")
-})
-
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { BrowserRouter , Routes , Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Profile from './pages/Profile'
-import SignUp from './pages/SignUp'
-import About from './pages/About'
-import SignIn from './pages/Signin'
-import Header from './components/Header'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import SignIn from './pages/Signin';
+import Header from './components/Header';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <BrowserRouter>
-      <Header/>
+    <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/About' element={<About/>}/>
-        <Route path='/SignIn' element={<SignIn/>}/>
-        <Route path='/SignUp' element={<SignUp/>}/>
-        <Route path='/Profile' element={<Profile/>}/>
-
-
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/SignIn" element={<SignIn />} />
+        <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/Profile" element={<Profile />} />
       </Routes>
-      </BrowserRouter>
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
