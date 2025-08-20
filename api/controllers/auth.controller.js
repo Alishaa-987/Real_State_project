68744645a88c4a0298d3361b8616f57061f69454
Alisha Fatima <alishafatima6768@gmail.com>
2025-08-21 00:38:34 +0500

    changes

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,10 +2,11 @@ import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs';
 
 export const signUp = async (req , res , next)=>{
+        console.log("Received body:" , req.body)
+    try{
     const {username , email , password} = req.body;
     const hashPassword = await bcryptjs.hashSync(password , 12);
     const newUser = new User ({username , email , password:hashPassword});
-    try{
         await newUser.save();
         res.status(201).json({
             success:true,
