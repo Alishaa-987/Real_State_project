b45ab5eb9062aaba0ec23e1fa6edc2452999d0e3
Alisha Fatima <alishafatima6768@gmail.com>
2025-08-30 02:42:29 +0500

    create search api rotur

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -4,7 +4,7 @@ export const createListing = async (req, res, next) => {
   try {
     const listing = await Listing.create({
       ...req.body,
-      userRef: req.user.id,   
+      userRef: req.user.id,
     });
 
     res.status(201).json(listing);
@@ -24,47 +24,98 @@ export const getAllListings = async (req, res, next) => {
   }
 };
 
-export const deleteListing = async (req , res , next)=>{
-    const listing = await Listing.findById(req.params.id);
-    if(!listing){
-        return next(errorHandler(404 , "listing not found"));
-    }
+export const deleteListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "listing not found"));
+  }
 
-    if (req.user.id != listing.userRef){
-        return next(errorHandler(401 , "you can only delete your own listing "));
-    
-    }
-    try{
-        await Listing.findByIdAndDelete(req.params.id);
-        res.status(200).json("listing has been deleted");
-    }catch(error){
-        next(error);
-    }
+  if (req.user.id != listing.userRef) {
+    return next(errorHandler(401, "you can only delete your own listing "));
+
+  }
+  try {
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json("listing has been deleted");
+  } catch (error) {
+    next(error);
+  }
 
 }
 
-export const updateListing = async (req , res , next)=>{
-   const listing = await Listing.findById(req.params.id);
-   if(!listing){
-    return next(errorHandler(404 , 'Listing not found'));
-   }
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, 'Listing not found'));
+  }
 
-   try{
-       const updatedListing = await Listing.findByIdAndUpdate(req.params.id, req.body, { new: true });
-       res.status(200).json(updatedListing);
-   }catch(error){
-       next(error);
-   }
-};
-
-export const getListing = async (req , res , next)=>{
-  try{
-    const listing = await Listing.findById(req.params.id);
-    if(!listing){
-      return next(errorHandler(404 , "Listing not found!"));
-    }
-    res.status(200).json(listing);
-  }catch(error){
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.status(200).json(updatedListing);
+  } catch (error) {
     next(error);
   }
 };
+export const getListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404, "Listing not found!"));
+    }
+    res.status(200).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 10;
+    const startIndex = parseInt((req.query.startIndex)) || 0;
+    let offer = req.query.offer;
+    if (offer === undefined || offer == 'false') {
+      offer = { $in: [false, true] }
+    }
+
+    let furnished = req.query.furnished;
+    if (furnished === undefined || furnished === 'false') {
+      furnished = { $in: [false, true] };
+    }
+
+    let parking = req.query.parking;
+    if (parking === undefined || parking === 'false') {
+      parking = { $in: [false, true] };
+    }
+
+    let pets = req.query.pets;
+    if (pets === undefined || pets === 'false') {
+      pets = { $in: [false, true] };
+    }
+    let type = req.query.type;
+    if (type === undefined || type === 'all') {
+      type = { $in: ['sale', 'rent'] };
+    }
+
+    const searchTerm = req.query.searchTerm || '';
+    const sort = req.query.sort || 'createdAt';
+    const order = req.query.order || 'desc';
+
+  
+    const listings = await Listing.find({
+      title: { $regex: searchTerm, $options: 'i' },
+      offer,
+      furnished,
+      parking,
+      type,
+
+    }).sort({
+      [sort]: order
+    }).limit(limit).skip(startIndex);
+
+    return res.status(200).json(listings);
+
+
+  } catch (error) {
+    next(error);
+  }
+}
