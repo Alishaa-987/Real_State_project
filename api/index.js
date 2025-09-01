78f327b882306d8e037318a5e01f848a010aafd3
Alisha Fatima <alishafatima6768@gmail.com>
2025-09-02 01:24:58 +0500

    port changes

--- api/index.js
+++ api/index.js
@@ -40,7 +40,7 @@ app.use(cookieParser());
 
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
